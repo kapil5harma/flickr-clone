607598fa22f710e8fc47977caee54f5e24739bea
Kapil Sharma <kapilsharmajmps@gmail.com>
2019-01-13 17:11:42 +0530

    Saga modified to fetch only 6 search results

--- src/app/groups/store/sagas.js
+++ src/app/groups/store/sagas.js
@@ -11,21 +11,17 @@ export function* fetchSearchResultsSaga(action) {
     yield put(Creators.fetchSearchResultsStart());
     const url = `${apiEndpoints.search}&api_key=${
       process.env.REACT_APP_FLICKR_KEY
-    }&text=${searchText}&format=json&nojsoncallback=1`;
-    // const url = yield apiEndpoints.gallery;
-    // const url = yield apiEndpoints.groupsBrowse;
-    console.log('url: ', url);
+    }&text=${searchText}&per_page=6&format=json&nojsoncallback=1`;
     const res = yield axios.post(url);
-    console.log('res: ', res);
+    // console.log('res: ', res);
     const groups = [];
     for (let key in res.data.groups.group) {
       groups.push({
         id: key,
         ...res.data.groups.group[key]
       });
-      if (key >= 5) break;
     }
-    console.log('groups: ', groups);
+    // console.log('groups: ', groups);
     yield put(Creators.fetchSearchResultsSuccess(groups));
   } catch (err) {
     yield put(Creators.fetchSearchResultsFailure(err));
